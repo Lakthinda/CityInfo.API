9b247ff0a01d2228c2a476d3b597fcea339663ee
Lak <lakthinda@gmail.com>
2018-09-08 23:27:45 +1000

    Added CitiesController, and PointOfInterestControllers and DTos

---
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.API.Controllers
+{
+    [Route("api/cities")]
+    public class CitiesController : Controller
+    {
+        [HttpGet()]
+        public IActionResult GetCities()
+        {
+            var cities = CitiesDataStore.Current.Cities;            
+            return Ok(cities);            
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetCity(int id)
+        {
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == id);
+            if(city == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(city);
+        }
+    }
+}
