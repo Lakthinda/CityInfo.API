658d7559cd8ac4af82a07cc4dfd1888b3ac9b7c9
Lak <lakthinda@gmail.com>
2018-09-09 02:04:07 +1000

    Update, Partial Updates added

--- CityInfo.API/Controllers/PointOfInterestController.cs
+++ CityInfo.API/Controllers/PointOfInterestController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CityInfo.API.Models;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -22,7 +24,7 @@ namespace CityInfo.API.Controllers
             return Ok(city.PointsOfInterest);
         }
 
-        [HttpGet("{cityId}/pointsofinterest/{id}")]
+        [HttpGet("{cityId}/pointofinterest/{id}", Name = "getPointOfInterest")]
         public IActionResult GetPointOfInterest(int cityId,int id)
         {
             var pointOfInterest = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId)
@@ -34,6 +36,132 @@ namespace CityInfo.API.Controllers
             }
 
             return Ok(pointOfInterest);
+        }
+
+        [HttpPost("{cityId}/pointofinterest")]
+        public IActionResult CreatePointOfInterest(int cityId,
+                                                    [FromBody] PointOfInterestForCreationDto pointOfInterest)
+        {
+            if(pointOfInterest == null)
+            {
+                return BadRequest();
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            if(city.PointsOfInterest.FirstOrDefault(p => p.Name.Contains(pointOfInterest.Name)) != null)
+            {
+                return BadRequest(new { ErrorMessage = "This point of interest already exists." });
+            }
+
+            // TO DO - Improve later
+            var maxPointOfInterestId = city.PointsOfInterest.Max(p => p.Id);
+
+            var finalPointOfInterest = new PointOfInterestDto()
+            {
+                Id = ++maxPointOfInterestId,
+                Name = pointOfInterest.Name,
+                Description = pointOfInterest.Description
+            };
+
+            city.PointsOfInterest.Add(finalPointOfInterest);
+
+            return CreatedAtRoute("getPointOfInterest",
+                                  new { cityId = cityId, id = finalPointOfInterest },
+                                  finalPointOfInterest);
+        }
+
+        [HttpPut("{cityId}/pointofinterest/{id}")]
+        public IActionResult UpdatePointOfInterest(int cityId,int id,
+                                [FromBody] PointOfInterestForUpdateDto pointOfInterestForUpdate)
+        {
+            if (pointOfInterestForUpdate == null)
+            {
+                return BadRequest();
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            var pointOfInterest = city.PointsOfInterest.FirstOrDefault(p => p.Id == id);
+            if (pointOfInterest == null)
+            {
+                return BadRequest(new { ErrorMessage = "This point of interest does not exist." });
+            }
+
+            pointOfInterest.Name = pointOfInterestForUpdate.Name;
+            pointOfInterest.Description = pointOfInterestForUpdate.Description;
+
+            return NoContent();
+        }
+
+        [HttpPatch("{cityId}/pointofinterest/{id}")]
+        public IActionResult PartiallyUpdatePointOfInterest(int cityId, int id,
+                                [FromBody] JsonPatchDocument<PointOfInterestForUpdateDto> patchDoc)
+        {
+            if (patchDoc == null)
+            {
+                return BadRequest();
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            var pointOfInterest = city.PointsOfInterest.FirstOrDefault(p => p.Id == id);
+            if (pointOfInterest == null)
+            {
+                return BadRequest(new { ErrorMessage = "This point of interest does not exist." });
+            }
+
+            var pointOfInterestToPatch = new PointOfInterestForUpdateDto()
+            {
+                Name = pointOfInterest.Name,
+                Description = pointOfInterest.Description
+            };
+
+            // Apply patch
+            patchDoc.ApplyTo(pointOfInterestToPatch, ModelState);
+
+            TryValidateModel(pointOfInterestToPatch);
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+
+            pointOfInterest.Name = pointOfInterestToPatch.Name;
+            pointOfInterest.Description = pointOfInterestToPatch.Description;
+            
+            return NoContent();
         }
     }
 }
